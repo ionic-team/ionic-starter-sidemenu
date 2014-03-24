498efeb07cfcc15103b9c8444655cb37c893d373
Max Lynch <max@drifty.com>
2014-03-24 17:51:59 -0500

    Cleaned up stuff

--- js/app.js
+++ js/app.js
@@ -9,58 +9,59 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    StatusBar.styleDefault();
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
   });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
+    .state('app', {
+      url: "/app",
       abstract: true,
-      templateUrl: "templates/tabs.html"
+      templateUrl: "templates/menu.html",
+      controller: 'AppCtrl'
     })
 
-    // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-      url: '/dash',
+    .state('app.search', {
+      url: "/search",
       views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
+        'menuContent' :{
+          templateUrl: "templates/search.html"
         }
       }
     })
 
-    .state('tab.friends', {
-      url: '/friends',
+    .state('app.browse', {
+      url: "/browse",
       views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
+        'menuContent' :{
+          templateUrl: "templates/browse.html"
         }
       }
     })
-    .state('tab.account', {
-      url: '/account',
+    .state('app.playlists', {
+      url: "/playlists",
       views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
+        'menuContent' :{
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
         }
       }
     })
 
+    .state('app.single', {
+      url: "/playlists/:playlistId",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/playlist.html",
+          controller: 'PlaylistCtrl'
+        }
+      }
+    });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
+  $urlRouterProvider.otherwise('/app/playlists');
 });
 
--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,18 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('AppCtrl', function($scope) {
 })
-.controller('FriendsCtrl', function($scope) {
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
 })
-.controller('AccountCtrl', function($scope) {
-});
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
