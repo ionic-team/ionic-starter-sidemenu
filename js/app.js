9809c24b1a51f5f9af12285484705608a0e0e80c
Max Lynch <max@drifty.com>
2014-05-14 10:30:39 -0500

    Keyboard bar

--- js/app.js
+++ js/app.js
@@ -8,6 +8,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
