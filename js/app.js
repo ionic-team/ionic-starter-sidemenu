fb2dc240a5f8daa879574a57e864e63c9e144eda
Alain Béarez <Alain@cuali.com.br>
2015-02-19 16:30:41 -0300

    fix(wp8): sanitize the ms-appx prefix WP8 adds
    See http://appfoundry.be/blog/2014/10/16/ionic-windows-phone/#ms-appx-ie-problem for an extended explanation.

--- js/app.js
+++ js/app.js
@@ -20,7 +20,9 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($compileProvider, $stateProvider, $urlRouterProvider) {
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|ghttps?|ms-appx|x-wmapp0):/) //WP8
+
   $stateProvider
 
   .state('app', {
