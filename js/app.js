fbbb6782a6ceb648ce6d2172e89bf280e22601bc
mhartington <mikehartington@gmail.com>
2015-07-14 12:54:40 -0400

    clean up

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -23,45 +25,45 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-  .state('app', {
-    url: "/app",
+    .state('app', {
+    url: '/app',
     abstract: true,
-    templateUrl: "templates/menu.html",
+    templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
   })
 
   .state('app.search', {
-    url: "/search",
+    url: '/search',
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: 'templates/search.html'
       }
     }
   })
 
   .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
+      url: '/browse',
       views: {
         'menuContent': {
-          templateUrl: "templates/playlists.html",
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
           controller: 'PlaylistsCtrl'
         }
       }
     })
 
   .state('app.single', {
-    url: "/playlists/:playlistId",
+    url: '/playlists/:playlistId',
     views: {
       'menuContent': {
-        templateUrl: "templates/playlist.html",
+        templateUrl: 'templates/playlist.html',
         controller: 'PlaylistCtrl'
       }
     }
--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,14 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  
+
   // Form data for the login modal
   $scope.loginData = {};
 
