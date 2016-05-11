580f51fac56fe04574b247b8c290830b0cff1bb6
Nadir Hassan <nadir@eventmobi.com>
2016-05-11 18:23:00 -0400

    Add intermediate check for plugins during bootstrap

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
